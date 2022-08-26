53e02e69dbf9f4d4ca2e3b25a9964da3f0a88841
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-08-25 23:51:33 -0400

    completed project

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,4 +25,48 @@ var changeColor = function () {
     }
 }
 
+$(".saveBtn").click(function() {
+    eventText = $(this).siblings(".textarea").val();
+    eventTime = $(this).siblings(".hour").text();
+
+    localStorage.setItem(eventTime, JSON.stringify(eventText));
+
+});
+
+    var saveEventText = JSON.parse(localStorage.getItem("9AM"));
+    $("#09").val("");
+    $("#09").val(saveEventText);
+
+    saveEventText = JSON.parse(localStorage.getItem("10AM"));
+    $("#10").val("");
+    $("#10").val(saveEventText);
+
+    saveEventText = JSON.parse(localStorage.getItem("11AM"));
+    $("#11").val("");
+    $("#11").val(saveEventText);
+
+    saveEventText = JSON.parse(localStorage.getItem("12AM"));
+    $("#12").val("");
+    $("#12").val(saveEventText);
+
+    saveEventText = JSON.parse(localStorage.getItem("1PM"));
+    $("#13").val("");
+    $("#13").val(saveEventText);
+
+    saveEventText = JSON.parse(localStorage.getItem("2PM"));
+    $("#14").val("");
+    $("#14").val(saveEventText);
+
+    saveEventText = JSON.parse(localStorage.getItem("3PM"));
+    $("#15").val("");
+    $("#15").val(saveEventText);
+
+    saveEventText = JSON.parse(localStorage.getItem("4PM"));
+    $("#16").val("");
+    $("#16").val(saveEventText);
+
+    saveEventText = JSON.parse(localStorage.getItem("5PM"));
+    $("#17").val("");
+    $("#17").val(saveEventText);
+
 changeColor();

fa17be969e2cba2ea115366285824d60c984f3a1
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-08-25 23:29:37 -0400

    color change working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,28 @@
 var dateTime = moment().format("dddd, MMMM Do, YYYY");
+var timeBlockEl = document.getElementsByClassName(".input");
 $("#currentDay").text(dateTime);
+
+
+var changeColor = function () {
+
+    var currentTime = moment().format('HH');
+    var timeBlockEl = $(".textarea");
+
+    for (var i = 0 ; i < timeBlockEl.length ; i++) {
+
+        var elementID = timeBlockEl[i].id;
+        var manipID = document.getElementById(timeBlockEl[i].id)
+
+        $(timeBlockEl[i].id).removeClass(".present .past .future");
+
+        if (elementID === currentTime) {
+            $(manipID).addClass("present");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("past");
+        }
+    }
+}
+
+changeColor();
